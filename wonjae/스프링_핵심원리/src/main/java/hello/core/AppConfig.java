c8e6ce7d4ad874ebfd99b2bb68fafc0f0cef575d
yyytir777_notebook <yyytir777@gmail.com>
2023-10-13 15:44:06 +0900

    3주차 정리

---
+++ wonjae/스프링_핵심원리/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,56 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+    // -> 같은 클래스가 두번 생성
+
+    //예상
+//    call AppConfig.memberService
+//    call AppConfig.memberRepository
+//    call AppConfig.memberRepository
+//    call AppConfig.orderService
+//    call AppConfig.memberRepository
+
+    //실제
+//    call AppConfig.memberService
+//    call AppConfig.memberRepository
+//    call AppConfig.orderService
+    @Bean
+    public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
