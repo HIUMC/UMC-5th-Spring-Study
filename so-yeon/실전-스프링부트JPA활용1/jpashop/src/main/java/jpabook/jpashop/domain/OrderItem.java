f5de9238395821a78f7e03614122ac3e6e136a3b
soyeoneeii <syp0463@gmail.com>
2023-10-31 18:21:27 +0900

    박소연-핵심원리마무리&JPA활용1 섹션2까지

---
+++ so-yeon/실전-스프링부트JPA활용1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="order_item")
+@Getter @Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; //주문 당시 가격
+    private int count; //주문 수량
+
+}
