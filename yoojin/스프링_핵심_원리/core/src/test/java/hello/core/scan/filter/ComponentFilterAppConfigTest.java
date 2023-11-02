d1650d4c16813f0205866a5d71378ad79cfb5816
u-genuine <u_genuine@naver.com>
2023-11-02 17:24:06 +0900

    4주차 실습

--- yoojin/스프링_핵심_원리/core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ yoojin/스프링_핵심_원리/core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -3,20 +3,23 @@ package hello.core.scan.filter;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.context.annotation.ComponentScan.*;
+
 
 public class ComponentFilterAppConfigTest {
 
     @Test
     void filterScan(){
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
-        BeanA beanA = ac.getBean("BeanA", BeanA.class);
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
         assertThat(beanA).isNotNull();
 
 
